8f32e425269927182db6faafe7fc2e0155b2bb2c
Chijiang <chijiang93@gmail.com>
2019-02-28 17:10:46 +0100

    Adding the corrdinate coverting function

---
+++ CameraCoorToRGB.m
@@ -0,0 +1,12 @@
+function [Pcx,Pcy] = CameraCoorToRGB(WeldingPos,Constants)
+Fdx = Constants.CameraParameters.Fdx; %367.286994
+Fdy = Constants.CameraParameters.Fdy; %367.286855
+Cdx = Constants.CameraParameters.Cdx; %255.16569
+Cdy = Constants.CameraParameters.Cdy; %211.82460
+
+Pdy = ((WeldingPos(1,2)*Fdy)/WeldingPos(1,3))+Cdy;
+Pdx = ((WeldingPos(1,1)*Fdx)/WeldingPos(1,3))+Cdx;
+
+Pcx = round(Pdx*2.9111); 
+Pcy = round(Pdy*2.9316);
+end 
