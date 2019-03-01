c879268858c1216c3118b768540079ed54fef25f
chijiang <chijiang@gmail.com>
2019-03-01 23:48:02 +0100

    Further documentation adding.

--- calculate_ptCloud.m
+++ calculate_ptCloud.m
@@ -1,33 +1,69 @@
 function ptCloud = calculate_ptCloud(depth, centroid, bbox, pointcloud)   
-    if size(centroid, 1) == 1
+    % calculate_ptCloud - Calculate the pointCloud of target
+	% 	object by using the depth information.
+    %
+    % Syntax:  
+	%		ptCloud = calculate_ptCloud(depth, centroid, bbox, pointcloud)
+	%
+	% Inputs:
+    %    depth - The realsense.depth_frame object, carrying 
+	%				depth information.
+    %    centroid - The center location of the ROI in pixel 
+	%				scale. 
+	%	 bbox - Boundary of the ROI in pixel scale.
+	%	 pointcloud - The realsense.pointcloud object, which
+	%				contains method to calculate the point 
+	%				cloud with realsense.depth_frame object.
+	%
+    % Outputs:
+    %    ptCloud - realsense.pointCloud object, inheritance
+	%	 			class of matlab pointCloud class.
+    %
+    % Author: Chijiang Duan
+    % email: chijiang.duan@tu-braunschweig.de
+    % Mar 2019; Version 1.0.0
+	%------------- BEGIN CODE --------------
+	
+	% Initialize the point cloud.
+	ptCloud = [];
+	
+	% Verify if the only ROI has been detected.
+	if size(centroid, 1) == 1
+		% Calculating of the points of the point cloud.
         points = pointcloud.calculate(depth);
         vertices = points.get_vertices();
-        ptCloud = pointCloud(vertices);
-        bbox = double(bbox);
+        % Create the pointCloud object out of the points.
+		ptCloud = pointCloud(vertices);
+        
+		% Calculation of Conversion the ROI boundary in 
+		% pixel scale to the real world scale (meter).
+		% The parameters are calibrated.
+		bbox = double(bbox);
         scale1 = depth.get_distance(bbox(1), bbox(2));
-        scale2 = depth.get_distance(bbox(1)+bbox(3), bbox(2) + bbox(4));
-        u_l_corner = [(bbox(1) - 640) * scale1/(1280/(2 * tan(65/180*pi/2))), -(720 - bbox(2) - bbox(4) - 360) * scale1/(720/(2 * tan(38/180*pi/2)))];
-        d_r_corner = [(bbox(1) + bbox(3) - 640) * scale2/(1280/(2 * tan(73.8/180*pi/2))), -(720 - bbox(2) - 360) * scale2/(1280/(2 * tan(57.5/180*pi/2)))];
-%         xlim = ptCloud.XLimits;
-%         ylim = ptCloud.YLimits;
-%         picsize = size(crop_depth);
-%         Xscale = (xlim(2) - xlim(1)) / picsize(2);
-%         Yscale = (ylim(2) - ylim(1)) / picsize(1);
-%         ROIcoor = [[bbox(1), bbox(1)+bbox(3)]*Xscale + xlim(1);...
-%             [ylim(2)/Yscale - bbox(2) - bbox(4), ylim(2)/Yscale - bbox(2)]*Yscale ; -Inf, Inf];
-% %         obj_dist = depth.get_distance(round(centroid(1)), round(centroid(2)));
-% %         ROIcoor = [-Inf, Inf; -Inf, Inf; 0.95*obj_dist, 1.05*obj_dist];
-        ROIcoor = [u_l_corner(1), d_r_corner(1); d_r_corner(2), u_l_corner(2); 0.8*scale1, 1.2*scale1];
-        sampleIndices = findPointsInROI(ptCloud, ROIcoor);
+        scale2 = depth.get_distance(bbox(1)+bbox(3),...
+			bbox(2) + bbox(4));
+		% Position of the upper left corner.
+        u_l_corner = [(bbox(1) - 640) * scale1/(1280/...
+			(2 * tan(65/180*pi/2))), -(720 - bbox(2)...
+				- bbox(4) - 360) * scale1/(720/...
+					(2 * tan(38/180*pi/2)))];
+		% Position of the lower right corner.
+        l_r_corner = [(bbox(1) + bbox(3) - 640) * ...
+			scale2/(1280/(2 * tan(73.8/180*pi/2))),...
+				-(720 - bbox(2) - 360) * scale2/(1280/...
+					(2 * tan(57.5/180*pi/2)))];
+		
+		% The ROI in 3D.
+        ROIcoor = [u_l_corner(1), l_r_corner(1); ...
+			l_r_corner(2), u_l_corner(2); ...
+				0.8*scale1, 1.2*scale1];
+        
+		% Crop the ROI point cloud out of the entire 
+		% point cloud. 
+		sampleIndices = findPointsInROI(ptCloud, ROIcoor);
         ptCloud = select(ptCloud,sampleIndices); 
+		% Remove unnecessary points.
         ptCloud = pcdenoise(ptCloud, 'Threshold',0.05);
     end
-%     xyzpoints = double(cat(3,X_coor,Y_coor,Z_coor));
-%     ptCloud = pointCloud(xyzpoints);
-%     sampleIndices = findPointsInROI(ptCloud,Constants.ROI_XYZ); 
-%     ptCloud = select(ptCloud,sampleIndices); 
-    % ptCloud = pcdenoise(ptCloud,'NumNeighbors',30,'Threshold',0.25); 
-    % T = affine3d([1,0,0,0;0,cos(pi),sin(pi),0;0,-sin(pi),cos(pi),0;0,0,ptCloud.ZLimits(1,1),1]);
-    % ptCloud = pctransform(ptCloud,T);
-   
+	%------------- END OF CODE --------------
 end
--- icp_Classification.m
+++ icp_Classification.m
@@ -1,42 +1,67 @@
-function [errValue, ObjectID] = icp_Classification(ptCloud,Referenzdatenbank,Constants)
-    FieldNames = fieldnames(Referenzdatenbank);
-    errValueArray = zeros(numel(FieldNames),1);
+function [errValue, objectID] = icp_Classification(ptCloud,Referenzdatenbank,Constants)
+    % icp_Classification - Classification function
+	% 	using the point cloud of workpiece and standard
+	%	model with ICP algorithms.
+    %
+    % Syntax:  
+	%	[errValue, objectID] = icp_Classification(ptCloud,Referenzdatenbank,Constants)
+	%
+	% Inputs:
+    %	 ptCloud - Point cloud of the workpiece.
+	%	 Referenzdatenbank - Data bank contains the 
+	%			point clouds of the standard models.
+	%	 Constants - Structure contains all variables
+	%			for the program.
+	%
+    % Outputs:
+    %    errValue - Root mean squared error, error of 
+	%			the classification.
+	%	 objectID - Classified ID of the workpiece.
+    %
+    % Author: Chijiang Duan
+    % email: chijiang.duan@tu-braunschweig.de
+    % Mar 2019; Version 1.0.0
+    %------------- BEGIN CODE --------------
+	workpieces = fieldnames(Referenzdatenbank);
+    errValueArray = zeros(numel(workpieces),1);
 
-    for idx = 1 :numel(FieldNames)
-        Center = Referenzdatenbank.(FieldNames{idx}).Center;
-        ReferencePtCloud = Referenzdatenbank.(FieldNames{idx}).ptCloud;
+    for idx = 1 :numel(workpieces)
+        center = Referenzdatenbank.(workpieces{idx}).Center;
+        reference_PtCloud = Referenzdatenbank.(workpieces{idx}).ptCloud;
 
         rotx = [1, 0, 0, 0; 0, cos(pi), -sin(pi), 0; 0, sin(pi), cos(pi), 0; 0, 0, 0, 1];
         rotz = [cos(-pi/2), -sin(-pi/2), 0, 0; sin(-pi/2), cos(-pi/2), 0, 0; 0, 0, 1, 0; 0, 0, 0, 1];
         rotation = (rotx*rotz)';
-        T_trans = affine3d(rotation);
-        ptCloud_resize = pctransform(ptCloud,T_trans); 
+        t_trans = affine3d(rotation);
+        ptCloud_resize = pctransform(ptCloud,t_trans); 
         
-        Xsize = (ptCloud_resize.XLimits(1,2) - ptCloud_resize.XLimits(1,1)) / ...
-            (ReferencePtCloud.XLimits(1,2) - ReferencePtCloud.XLimits(1,1));
-        Ysize = (ptCloud_resize.YLimits(1,2) - ptCloud_resize.YLimits(1,1)) / ...
-            (ReferencePtCloud.YLimits(1,2) - ReferencePtCloud.YLimits(1,1));
-        Zsize = (ptCloud_resize.ZLimits(1,2) - ptCloud_resize.ZLimits(1,1)) / ...
-            (ReferencePtCloud.ZLimits(1,2) - ReferencePtCloud.ZLimits(1,1));
+        x_size = (ptCloud_resize.XLimits(1,2) - ptCloud_resize.XLimits(1,1)) / ...
+            (reference_PtCloud.XLimits(1,2) - reference_PtCloud.XLimits(1,1));
+        y_size = (ptCloud_resize.YLimits(1,2) - ptCloud_resize.YLimits(1,1)) / ...
+            (reference_PtCloud.YLimits(1,2) - reference_PtCloud.YLimits(1,1));
+        z_size = (ptCloud_resize.ZLimits(1,2) - ptCloud_resize.ZLimits(1,1)) / ...
+            (reference_PtCloud.ZLimits(1,2) - reference_PtCloud.ZLimits(1,1));
         
-        trans = [1/Xsize,0,0,0;0,1/Ysize,0,0;0,0,1/Zsize,0;0,0,0,1];
-        T_trans = affine3d(trans);
-        ptCloud_resize = pctransform(ptCloud_resize,T_trans); 
+        trans = [1/x_size,0,0,0;0,1/y_size,0,0;0,0,1/z_size,0;0,0,0,1];
+        t_trans = affine3d(trans);
+        ptCloud_resize = pctransform(ptCloud_resize,t_trans); 
 
-        Xdiff = ((ptCloud_resize.XLimits(1,2)-ptCloud_resize.XLimits(1,1))/2)+...
-                ptCloud_resize.XLimits(1,1)-Center(1,1);
-        Ydiff = ((ptCloud_resize.YLimits(1,2)-ptCloud_resize.YLimits(1,1))/2)+...
-                ptCloud_resize.YLimits(1,1)-Center(1,2);
-        Zdiff = ((ptCloud_resize.ZLimits(1,2)-ptCloud_resize.ZLimits(1,1))/2)+...
-                ptCloud_resize.ZLimits(1,1)-Center(1,3);
-        T_trans = affine3d([1,0,0,0;0,1,0,0;0,0,1,0;Xdiff,Ydiff,Zdiff,1]);
-        ReferencePtCloud = pctransform(ReferencePtCloud,T_trans); 
+        x_diff = ((ptCloud_resize.XLimits(1,2)-ptCloud_resize.XLimits(1,1))/2)+...
+                ptCloud_resize.XLimits(1,1)-center(1,1);
+        y_diff = ((ptCloud_resize.YLimits(1,2)-ptCloud_resize.YLimits(1,1))/2)+...
+                ptCloud_resize.YLimits(1,1)-center(1,2);
+        z_diff = ((ptCloud_resize.ZLimits(1,2)-ptCloud_resize.ZLimits(1,1))/2)+...
+                ptCloud_resize.ZLimits(1,1)-center(1,3);
+        t_trans = affine3d([1,0,0,0;0,1,0,0;0,0,1,0;x_diff,y_diff,z_diff,1]);
+        reference_PtCloud = pctransform(reference_PtCloud,t_trans); 
 
-        [~,PC_ICP,rmse] = pcregistericp(ReferencePtCloud,ptCloud_resize,'MaxIterations',Constants.ICP_Parameters.ICP_Classi_iter,...
+        [~,pc_ICP,rmse] = pcregistericp(reference_PtCloud,ptCloud_resize,'MaxIterations',Constants.ICP_Parameters.ICP_Classi_iter,...
             'metric','pointToPlane');
         errValueArray(idx) = rmse;
 %         figure();
-%         pcshow(ReferencePtCloud); hold on; pcshow(ptCloud_resize);hold off;
+%         pcshow(reference_PtCloud); hold on; pcshow(ptCloud_resize);hold off;
     end 
-    [errValue,ObjectID] = min(errValueArray);
+    [errValue,objectID] = min(errValueArray);
+	
+	%------------- END OF CODE --------------
 end
