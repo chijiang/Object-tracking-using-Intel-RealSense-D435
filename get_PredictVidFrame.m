a10242c60c8e98580b5d539e385e9265f10f3ed2
chijiang <chijiang@gmail.com>
2019-03-07 17:49:51 +0100

    Changes in position prediction function

--- get_PredictVidFrame.m
+++ get_PredictVidFrame.m
@@ -1,8 +1,32 @@
-function PredictionVidFrame = get_PredictVidFrame...
-    (croped_color,velocity_vector,weldingAverageArray,LastTime,LastCenterLoc,T,Constants)
-    Tdiff = T-LastTime;
-    NewPos = (weldingAverageArray+LastCenterLoc) + (velocity_vector*Tdiff);
-    [Pcx,Pcy] = CameraCoorToRGB(NewPos,Constants);
+function prediction_VidFrame = predicted_VidFrame...
+    (color_img,velocity_vector,weldingAverageArray,finish_time,center_location_finish,t)
+	% predicted_VidFrame - Calculate the video frame that contains
+	% 	the predicted welding positions marked.
+    %
+    % Syntax:  
+	%	prediction_VidFrame = predicted_VidFrame...
+    %		(color_img,velocity_vector,weldingAverageArray,finish_time,center_location_finish,t)
+	%
+	% Inputs:
+    %	 color_img - The RGB image taken by the camera.
+	%	 velocity_vector - The velocity vector by cakculation.
+	%	 weldingAverageArray - The average vector shows the direction
+	%	 	of the welding position change.
+	%	 finish_time - The end coordinate of the welding position
+	%		recorded.
+	%
+    % Outputs:
+    %    errValue - Root mean squared error, error of 
+	%			the classification.
+	%	 objectID - Classified ID of the workpiece.
+    %
+    % Author: Chijiang Duan
+    % email: chijiang.duan@tu-braunschweig.de
+    % Mar 2019; Version 1.0.0
+    %------------- BEGIN CODE --------------
+    t_diff = t-finish_time;
+    next_pos = (weldingAverageArray+center_location_finish) + (velocity_vector*t_diff);
+    [pixel_x,pixel_y] = camCoor2RGB(next_pos);
     
-    PredictionVidFrame = insertShape(croped_color,'circle',[Pcx Pcy 10],'LineWidth',5,'color','red');
+    prediction_VidFrame = insertShape(color_img,'circle',[Pcx Pcy 10],'LineWidth',5,'color','red');
 end
